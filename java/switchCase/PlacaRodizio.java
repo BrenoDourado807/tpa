b498221700d0dc5d9f5b951f1dd6577026c89f13
BrenoDourado807 <brenosilv807@gmail.com>
2025-10-21 16:36:26 -0300

    Add files via upload

---
+++ java/switchCase/PlacaRodizio.java
@@ -0,0 +1,39 @@
+package switchCase;
+
+import java.util.Scanner;
+
+public class PlacaRodizio {
+public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		 int m;
+		 
+		 System.out.println("digite o número da placa (0 a 9)");
+		 m = ler.nextInt();
+		 
+		 switch (m) {
+		 
+		 	case 1:
+		 	case 2:
+		 		System.out.println("Segunda-feira");
+		 		break;
+		 	case 3:
+		 	case 4:	
+		 		System.out.println("Terça-feira");
+		 		break;
+		 	case 5:
+		 	case 6:
+		 		System.out.println("Quarta-feira");
+		 		break;
+		 	case 7:
+		 	case 8:
+		 		System.out.println("Quinta-feira");
+		 		break;
+		 	case 9:
+		 	case 0:
+		 		System.out.println("sexta-eira");
+		 } 
+		 ler.close();
+	}
+}
